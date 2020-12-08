d4b6ba8ec0fb02c101be5a9c041feecc3fdc198b
Eduardo-SantosS <santos.eduardopro@gmail.com>
2020-12-08 14:12:11 -0300

    Project created

---
+++ src/main/java/com/spring/jpa/WebServiceG/WebServiceGApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.WebServiceG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceGApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jpa/WebServiceG/WebServiceGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.WebServiceG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
